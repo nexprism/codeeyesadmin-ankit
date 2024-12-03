003cbda227fc1331c97bc3a705c2769549051283
Ankit Kumar Upadhyay <ankitupadhyay4519@gmail.com>
2024-12-03 15:10:24 +0530

    last

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,9 +1,10 @@
 // export const API_BASE_URL = "https://devvrat-new-backend.onrender.com/api/admin";
-export const API_BASE_URL = "http://localhost:8000/api/admin";
-export const LOCAL_API_BASE_URL = "http://localhost:8000/api/admin";
+// export const API_BASE_URL = "http://localhost:8000/api/admin";
+// export const LOCAL_API_BASE_URL = "http://localhost:8000/api/admin";
 // export const API_BASE_URL = "https://codeyes-server.vercel.app/api/admin/";
 
-// export const LOCAL_API_BASE_URL ="https://codeyes-server.vercel.app/api/admin/";
+export const LOCAL_API_BASE_URL ="https://codeyes-server.vercel.app/api/admin/";
+export const API_BASE_URL ="https://codeyes-server.vercel.app/api/admin/";
 
 // export const API_BASE_URL = "https://everythinge.nexprism.in/api/admin/";
 // export const API_BASE_URL = "https://everythinge.nexprism.in/api/admin/";
